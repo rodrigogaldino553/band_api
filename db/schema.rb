4a6d1738f81e383b17c45e55ffe099a92d440a23
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2022-11-13 23:10:22 -0300

    add serializers

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_14_013337) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_14_014553) do
   create_table "bands", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "members", force: :cascade do |t|
+    t.integer "band_id", null: false
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["band_id"], name: "index_members_on_band_id"
+  end
+
+  add_foreign_key "members", "bands"
 end
